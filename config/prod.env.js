2b01f878d3b9bcef7ce1aeb7db076297b82bea11
lwyj123 <443474713@qq.com>
2018-04-24 20:19:47 +0800

    fix: build fail bug

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     BASE_API: '"http://52.78.119.221:802/api/"',
-    SSO_SITE: '"http://52.78.119.221:801"',,
+    SSO_SITE: '"http://52.78.119.221:801"',
     APP_ORIGIN: '"http://wuanlife.com"',
 }
